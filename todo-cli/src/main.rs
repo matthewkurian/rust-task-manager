c4f0bbda4aa5da8209515e957bc24a20872ab7a8
Matthew Kurian <matthewwkurian@gmail.com>
2025-08-19 22:12:42 +1200

    Add initial Item struct

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,3 +1,11 @@
+
+struct Item {
+    id: i32,
+    desc: String,
+    done: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task1 = Item{id: 1, desc: "Pick up food".to_string(), done: false};
+    println!("{}", task1.desc);
 }
