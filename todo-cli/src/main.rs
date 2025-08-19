a8ac47c91a7bc3348d72b15291440565cfd0aea6
Matthew Kurian <matthewwkurian@gmail.com>
2025-08-19 22:31:21 +1200

    Add function to serialize task

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,3 +1,5 @@
+use serde::{Serialize, Deserialize};
+#[derive(serde::Serialize)]
 
 struct Item {
     id: i32,
@@ -5,13 +7,18 @@ struct Item {
     done: bool
 }
 
+fn save_task(task: Item) {
+    let json = serde_json::to_string_pretty(&task).unwrap();
+    println!("New Task Added \n{}", json)
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let option: &String = &args[1];
-    let item: &String = &args[2];
+    let item = Item{id: 1, desc: args[2].clone(), done: false};
 
     if option.to_lowercase() == "add" {
-        println!("Add Triggered: {}", item)
+        save_task(item);
     } else if option.to_lowercase() == "list" {
         println!("List Triggered")
     } else {
